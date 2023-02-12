53d7a363921528dd963a2f982dad7ff70bc35ef4
andregraciano16 <andregraciano16@gmail.com>
2023-02-12 19:31:40 -0300

    criando classe para genenciar as exceções

---
+++ src/main/java/br/com/vr/autorizador/config/exception/CustomExceptionHandler.java
@@ -0,0 +1,51 @@
+package br.com.vr.autorizador.config.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import br.com.vr.autorizador.exception.CartaoInexistenteException;
+import br.com.vr.autorizador.exception.CartaoJaExisteException;
+import br.com.vr.autorizador.exception.CartaoNaoEncontradoException;
+import br.com.vr.autorizador.exception.SaldoInsuficienteException;
+import br.com.vr.autorizador.exception.SenhaInvalidaException;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+	@ExceptionHandler(CartaoNaoEncontradoException.class)
+	public ResponseEntity<Object> handleUCartaoNaoEncontradoException(CartaoNaoEncontradoException exception,
+			WebRequest request) {
+
+		return new ResponseEntity<>(exception.getHttpStatus());
+	}
+
+	@ExceptionHandler(CartaoJaExisteException.class)
+	public ResponseEntity<Object> handleCartaoJaExisteException(CartaoJaExisteException exception,
+			WebRequest request) {
+
+		return new ResponseEntity<>(exception.getErroDetail(), exception.getHttpStatus());
+	}
+
+	@ExceptionHandler(CartaoInexistenteException.class)
+	public ResponseEntity<Object> handleUserNotFoundException(CartaoInexistenteException exception,
+			WebRequest request) {
+
+		return new ResponseEntity<>(exception.getMensagemErroEnum(), exception.getHttpStatus());
+	}
+
+	@ExceptionHandler(SaldoInsuficienteException.class)
+	public ResponseEntity<Object> handleSaldoInsuficienteException(SaldoInsuficienteException exception,
+			WebRequest request) {
+
+		return new ResponseEntity<>(exception.getMensagemErroEnum(), exception.getHttpStatus());
+	}
+
+	@ExceptionHandler(SenhaInvalidaException.class)
+	public ResponseEntity<Object> handleSenhaInvalidaException(SenhaInvalidaException exception,
+			WebRequest request) {
+		return new ResponseEntity<>(exception.getMensagemErroEnum(), exception.getHttpStatus());
+	}
+
+}
